5ae385fc96b247c22c93e2cadcd0203484e0aa6d
ilyalozinskiy <inbtrhope@gmail.com>
2024-12-03 14:21:20 +0400

    We can parse comma list

--- src/main/java/com/ecwid/parser/crawler/ColumnCrawler.java
+++ src/main/java/com/ecwid/parser/crawler/ColumnCrawler.java
@@ -1,13 +1,21 @@
 package com.ecwid.parser.crawler;
 
 import com.ecwid.parser.fragment.enity.Column;
+import com.ecwid.parser.fragment.enity.Query;
 import org.springframework.stereotype.Component;
 
+import java.util.function.BiConsumer;
+
 @Component
-public class ColumnCrawler extends SectionAwareCrawler {
+public class ColumnCrawler extends SectionAwareCrawler implements ListCrawler {
 
     {
         crawlUntil = fragment -> false;
-        addToQuery = (query, fragment) -> query.getColumns().add(new Column(fragment, null));
+        addToQuery = ListCrawler.super.addToQuery();
+    }
+
+    @Override
+    public BiConsumer<Query, String> onListItem() {
+        return (query, fragment) -> query.getColumns().add(new Column(fragment, null));
     }
 }
--- src/main/java/com/ecwid/parser/crawler/ListCrawler.java
+++ src/main/java/com/ecwid/parser/crawler/ListCrawler.java
@@ -1,5 +1,19 @@
 package com.ecwid.parser.crawler;
 
-public interface ListCrawler extends Crawler {
+import com.ecwid.parser.fragment.enity.Query;
 
+import java.util.Optional;
+import java.util.function.BiConsumer;
+
+import static com.ecwid.parser.Lexemes.LEX_COMMA;
+
+public interface ListCrawler {
+
+    BiConsumer<Query, String> onListItem();
+
+    default BiConsumer<Query, String> addToQuery() {
+        return (query, fragment) -> Optional.of(fragment)
+                .filter(f -> !LEX_COMMA.equals(f))
+                .ifPresent(f -> onListItem().accept(query, f));
+    }
 }

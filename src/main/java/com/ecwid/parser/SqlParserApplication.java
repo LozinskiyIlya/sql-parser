96cf18c4ffa195438574682d00a08e80c1d43c8e
ilyalozinskiy <inbtrhope@gmail.com>
2024-12-21 18:15:23 +0400

    refactor main

--- src/main/java/com/ecwid/parser/SqlParserApplication.java
+++ src/main/java/com/ecwid/parser/SqlParserApplication.java
@@ -9,28 +9,46 @@ import java.io.InputStreamReader;
 import java.io.PushbackReader;
 
 public class SqlParserApplication {
+    public static final int EXIT_CHAR_CODE = 113; // 'q'
 
     public static void main(String[] args) {
         final var context = new AnnotationConfigApplicationContext(ParserApplicationConfig.class);
         final var sqlParser = context.getBean(SqlParser.class);
         try (PushbackReader reader = new PushbackReader(new InputStreamReader(System.in))) {
             while (true) {
-                System.out.println("Enter a query to parse");
-                System.out.println(" - type 'q' to finish");
-                System.out.println(" - use ';' as a separator\n");
-
-                int c = reader.read();
-                if (c == 113) {
-                    // 'q'
-                    System.out.println("Bye!");
-                    break;
-                }
-                reader.unread(c);
+                printInstructions();
+                skipWhiteSpaces(reader);
+                checkExit(reader);
                 final var query = sqlParser.parse(reader);
-                System.out.println("Parsed query:\n" + QueryPrinter.printJson(query));
+                System.out.println("\nParsed query:\n" + QueryPrinter.printJson(query));
             }
         } catch (IOException e) {
             System.err.printf("Error while reading input %s\n", e.getMessage());
         }
+    }
+
+    private static void printInstructions() {
+        System.out.println("Enter a query to parse");
+        System.out.println(" - type ';' to start parsing");
+        System.out.println(" - type 'q' to finish");
+    }
+
+    private static void skipWhiteSpaces(PushbackReader reader) throws IOException {
+        int c;
+        while ((c = reader.read()) != -1) {
+            if (!Character.isWhitespace(c)) {
+                reader.unread(c);
+                break;
+            }
+        }
+    }
+
+    private static void checkExit(PushbackReader reader) throws IOException {
+        int c = reader.read();
+        if (c == EXIT_CHAR_CODE) {
+            System.out.println("Bye!");
+            System.exit(0);
+        }
+        reader.unread(c);
     }
 }
